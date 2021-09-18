34756356de6f0b464e9834bd34be812e6d880436
Glenn Nicoll <glennnicoll@gmail.com>
2021-09-18 22:18:57 +1000

    images and some styling begins

--- CodeChallengeGN.Website/Models/DrawResultModel.cs
+++ CodeChallengeGN.Website/Models/DrawResultModel.cs
@@ -23,6 +23,12 @@ namespace CodeChallengeGN.Website.Models
         public string DrawDisplayName { get; set; }
 
         public string DrawLogoUrl { get; set; }
+        public string DrawLogoSrcLocal { 
+            get
+            {
+                return "/images/" + ProductId.ToLower() + ".png";
+            }
+        }
 
         public IEnumerable<int> PrimaryNumbers { get; set; }
         public IEnumerable<int> SecondaryNumbers { get; set; }
@@ -30,7 +36,39 @@ namespace CodeChallengeGN.Website.Models
 
         public IEnumerable<DividendModel> Dividends { get; set; }
 
+        public int JackPotWinnersCount
+        {
+            get
+            {
+                if (Dividends.Any())
+                {
+                    var firstD = Dividends.First((d) => d.Division == 1);
+                    if (firstD != null)
+                    {
+                        return firstD.BlocNumberOfWinners;
+                    }
+                }
 
+                return 0;
+            }
+        }
+
+        public string JackPot
+        {
+            get
+            {
+                if (Dividends.Any())
+                {
+                    var firstD = Dividends.First((d)=>d.Division==1);
+                    if (firstD != null)
+                    {
+                        return "$"+String.Format("{0:n}", firstD.BlocDividend);
+                    }
+                }
+
+                return "-";
+            }
+        }
 
     }
 }
